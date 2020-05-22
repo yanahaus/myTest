05f59e586714cdf8ac56190c6babc52cc08c2a52
Yana Khaustova <yana.haus@gmail.com>
2020-05-22 12:15:50 +0300

    first task

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log('Hello!');
+﻿let money = prompt("Ваш бюджет на месяц?", "");
+let time = prompt("Введите дату в формате YYYY-MM-DD", "");
+
+let statya = prompt("Введите обязательную статью расходов в этом месяце", "");
+let summa = prompt("Во сколько обойдется?", "");
+let statya2 = prompt("Введите обязательную статью расходов в этом месяце", "");
+let summa2 = prompt("Во сколько обойдется?", "");
+
+let appData = {
+    budjet: money,
+    timeData: time,
+    expenses: { [statya] : summa, [statya2] : summa2 },
+    optionalExpenses: {},
+    income: [],
+    savings: false
+};
+console.log(appData);
+
+alert(`бюджет на 1 день = ${ money/30}`);
+
+
