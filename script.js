032516821a5710273d7312cb57997c4017130fc5
Yana Khaustova <yana.haus@gmail.com>
2020-07-31 09:31:34 +0300

    start2

--- script.js
+++ script.js
@@ -1,58 +1,18 @@
-﻿let money, time;
-function start() {
-  money = +prompt("Ваш бюджет на месяц?", "");
-  time = prompt("Введите дату в формате YYYY-MM-DD", "");
-  while (isNaN(money) || money == "" || money == null) {
-    money = +prompt("Ваш бюджет на месяц?", "");
-  }
-}
-start();
+﻿let money = prompt('Ваш бюджет на месяц?', '');
+let time = prompt('Введите дату в формате YYYY-MM-DD', '');
+
+let question1 = prompt('Введите обязательную статью расходов в этом месяце', '');
+let question2 = prompt('Во сколько обойдется?', '');
 
 let appData = {
   budjet: money,
   timeData: time,
-  expenses: {},
-  optionalExpenses: {},
+  optionalExpenses: {
+    question1: question2
+  },
   income: [],
   savings: false
 };
 
-function chooseExpenses() {
-  for (let i = 0; i < 2; i++) {
-    let a = prompt("Введите обязательную статью расходов в этом месяце", "");
-    let b = +prompt("Во сколько обойдется?", "");
-    if ((typeof (a)) === 'string' && (typeof (a)) != null && (typeof (b)) != null && a != '' && b != '') {
-      appData.expenses[a] = b;
-      console.log('done')
-    } else {
-      i--;
-    }
-  }
-}
-chooseExpenses();
-
-function detectDayBudget() {
-  appData.moneyPerDay = (appData.budjet / 30).toFixed(1);
-  alert("Ежедневный бюджет " + appData.moneyPerDay);
-}
-detectDayBudget();
-
-function checkSavings() {
-  if (appData.savings == true) {
-    let save = +prompt('Какова сумма накоплений', '');
-    let percent = +prompt('Под какой процент', '');
-    appData.monthIncome = save / 100 / 12 * percent;
-    alert('Доход в месяц с вашего депозита:' + appData.monthIncome);
-  }
-}
-checkSavings();
-
-
-function chooseOptExpenses () {
-  for (let i = 0; i < 3; i++) {
-    let c = prompt("Статья необязательных расходов?", "");
-    appData.optionalExpenses[i+1] = c;
-  }
-};
-chooseOptExpenses();
+alert(appData.budjet / 30);
 
